1bfb23e183d788c69643c336aec610f71993bd2d
80822215 <zztdandan@gmail.com>
2018-08-24 21:37:03 +0800

    new

--- src/components/user_case/CaseReview.vue
+++ src/components/user_case/CaseReview.vue
@@ -1,5 +1,5 @@
 <template>
-<!-- 根据输入的信息（由于是直接用路由加载，所以信息非常有限）获得当前项目情况及用户可以知道的信息，如果用户可以执行下一步则提供执行下一步的按钮-->
+    <!-- 根据输入的信息（由于是直接用路由加载，所以信息非常有限）获得当前项目情况及用户可以知道的信息，如果用户可以执行下一步则提供执行下一步的按钮-->
     <div>
 
     </div>
@@ -7,10 +7,22 @@
 
 <script>
     export default {
-        name:'case-review'
-    }
+      name: 'case-review',
+      data: function() {
+        return {
+          progress_id: '',
+          progress_info: {},
+          progress_user: '',
+          progress_next_node: {}
+        };
+      },
+      created: function() {
+        let that = this;
+        let progress_id=this.$route.progress_id;
+        this.$ajax.get('/api/user_view/case/case_progress?case_progress_id=')
+      }
+    };
 </script>
 
 <style scoped>
-
 </style>
